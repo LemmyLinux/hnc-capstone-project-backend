680b349783e1b40ebb2f17c2082230d5f36b4b16
Alba Rocamora <albamaria.rocamora@gmail.com>
2024-01-01 22:08:27 +0100

    Initialized backend project

---
+++ src/main/java/es/carlos3/rocamora/hernandez/pfcbackend/PfcBackendApplication.java
@@ -0,0 +1,13 @@
+package es.carlos3.rocamora.hernandez.pfcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/carlos3/rocamora/hernandez/pfcbackend/PfcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.carlos3.rocamora.hernandez.pfcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
