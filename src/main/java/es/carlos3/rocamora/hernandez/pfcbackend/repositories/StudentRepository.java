1cac30625fb280c34be3c4bbe5ce8fe3a22f0305
Alba Rocamora <albamaria.rocamora@gmail.com>
2024-05-29 20:55:56 +0200

    Implemented login endpoints and added booking ownership functionality

--- src/main/java/es/carlos3/rocamora/hernandez/pfcbackend/repositories/StudentRepository.java
+++ src/main/java/es/carlos3/rocamora/hernandez/pfcbackend/repositories/StudentRepository.java
@@ -1,9 +1,20 @@
 package es.carlos3.rocamora.hernandez.pfcbackend.repositories;
 
+import es.carlos3.rocamora.hernandez.pfcbackend.model.Booking;
+import es.carlos3.rocamora.hernandez.pfcbackend.model.Login;
 import es.carlos3.rocamora.hernandez.pfcbackend.model.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Repositorio de estudiantes. Esta clase gestiona los Students con la base de datos.
  */
-public interface StudentRepository extends CrudRepository<Student, Long> {}
+public interface StudentRepository extends CrudRepository<Student, Long> {
+    public List<Booking> findBookingsById(long id);
+
+    public Student findById(long id);
+
+    public Login findLoginById(long id);
+}
